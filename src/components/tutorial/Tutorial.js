581e448db5a5b06041e6b60e935ea5b5dfa84a7f
Adixxor <adrianna.woinska@gmail.com>
2021-05-03 18:50:09 +0200

    tutorial done

--- src/components/tutorial/Tutorial.js
+++ src/components/tutorial/Tutorial.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import TodoList from "./TodoList";
+
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 export default function Tutorial() {
-  return <div style={{ display: "flex", justifyContent: "center" }}>penis</div>;
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: Date.now(), name: name, complete: false }];
+    });
+    todoNameRef.current.value = null;
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter((todo) => !todo.complete);
+    setTodos(newTodos);
+  }
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
+        <input ref={todoNameRef} type="text" />
+        <button onClick={handleAddTodo}>Add Todo</button>
+        <button onClick={handleClearTodos}>Clear Completed</button>
+        <div>{todos.filter((todo) => !todo.complete).length} left to do</div>
+      </div>
+    </div>
+  );
 }
