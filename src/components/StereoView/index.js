9e17532777d014c83eba158abbaa8a1c72e18c4c
Adixxor <adrianna.woinska@gmail.com>
2021-06-20 21:17:16 +0200

    Add SwitchStereoViewTypeControl

--- src/components/StereoView/index.js
+++ src/components/StereoView/index.js
@@ -1,7 +1,8 @@
-import React, { useContext, useEffect, useState } from "react";
-import { ZoomControl } from "react-leaflet";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { ZoomControl, FeatureGroup } from "react-leaflet";
 import LeafletMap from "../LeafletMap";
 import { MdArrowBack } from "react-icons/md";
+import { HiOutlineSwitchHorizontal } from "react-icons/hi";
 import "leaflet.sync";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
@@ -43,12 +44,46 @@ const BackToMainPageControl = () => {
   );
 };
 
+const SwitchStereoViewTypeButton = styled.button`
+  position: absolute;
+  top: 90px;
+  right: 10px;
+  width: 34px;
+  height: 34px;
+  background-color: white;
+  border-radius: 4px;
+  border: 2px solid rgba(0, 0, 0, 0.25);
+  z-index: 1000;
+  text-align: center;
+
+  &:hover {
+    background-color: #f4f4f4;
+  }
+`;
+const SwitchStereoViewTypeIcon = styled(HiOutlineSwitchHorizontal)`
+  color: black;
+  height: 18px;
+  width: 18px;
+  margin-top: 5px;
+`;
+
+const SwitchStereoViewTypeControl = (props) => {
+  return (
+    <SwitchStereoViewTypeButton onClick={props.onClick}>
+      <SwitchStereoViewTypeIcon />
+    </SwitchStereoViewTypeButton>
+  );
+};
+
 // TODO:dodaj opcję pełnego ekranu
 
 export default function StereoView() {
   const [leftMap, setLeftMap] = useState();
   const [rightMap, setRightMap] = useState();
+  const [isReversed, setIsReversed] = useState(false);
   const { chosenStereopair } = useContext(AppContext);
+  const leftImageRef = useRef();
+  const rightImageRef = useRef();
 
   useEffect(() => {
     if (leftMap && rightMap) {
@@ -59,28 +94,39 @@ export default function StereoView() {
 
   useEffect(() => {
     if (leftMap && rightMap && chosenStereopair) {
-      const leftImage = L.imageOverlay
-        .rotated(
-          `${PUBLIC_IMAGES}${chosenStereopair.left.fileName}`,
-          new LatLng(...chosenStereopair.left.bounds[0]),
-          new LatLng(...chosenStereopair.left.bounds[1]),
-          new LatLng(...chosenStereopair.left.bounds[3])
-        )
-        .addTo(leftMap);
+      const leftImage = L.imageOverlay.rotated(
+        `${PUBLIC_IMAGES}${chosenStereopair.left.fileName}`,
+        new LatLng(...chosenStereopair.left.bounds[0]),
+        new LatLng(...chosenStereopair.left.bounds[1]),
+        new LatLng(...chosenStereopair.left.bounds[3])
+      );
 
-      L.imageOverlay
-        .rotated(
-          `${PUBLIC_IMAGES}${chosenStereopair.right.fileName}`,
-          new LatLng(...chosenStereopair.right.bounds[0]),
-          new LatLng(...chosenStereopair.right.bounds[1]),
-          new LatLng(...chosenStereopair.right.bounds[3])
-        )
-        .addTo(rightMap);
+      const rightImage = L.imageOverlay.rotated(
+        `${PUBLIC_IMAGES}${chosenStereopair.right.fileName}`,
+        new LatLng(...chosenStereopair.right.bounds[0]),
+        new LatLng(...chosenStereopair.right.bounds[1]),
+        new LatLng(...chosenStereopair.right.bounds[3])
+      );
+
+      leftImageRef.current.clearLayers();
+      rightImageRef.current.clearLayers();
+
+      if (isReversed) {
+        leftImage.addTo(rightImageRef.current);
+        rightImage.addTo(leftImageRef.current);
+      } else {
+        leftImage.addTo(leftImageRef.current);
+        rightImage.addTo(rightImageRef.current);
+      }
 
       // Wycentruj widok lewej mapy do bounds lewego zdjęcia. Dzięki synchronizowaniu mapek wystarczy centrowanie jednego widoku.
       leftMap.fitBounds(leftImage.getBounds());
     }
-  }, [leftMap, rightMap, chosenStereopair]);
+  }, [leftMap, rightMap, chosenStereopair, isReversed]);
+
+  function handleSwitchStereoViewClick() {
+    setIsReversed(!isReversed);
+  }
 
   return (
     <div style={{ display: "flex", width: "100%", height: "100vh" }}>
@@ -90,6 +136,10 @@ export default function StereoView() {
       >
         <ZoomControl position={"topright"} />
         <BackToMainPageControl />
+        <SwitchStereoViewTypeControl
+          onClick={() => handleSwitchStereoViewClick()}
+        />
+        <FeatureGroup ref={leftImageRef} />
       </LeafletMap>
       <LeafletMap
         style={{ width: "50%", height: "100vh" }}
@@ -97,6 +147,10 @@ export default function StereoView() {
       >
         <ZoomControl position={"topright"} />
         <BackToMainPageControl />
+        <SwitchStereoViewTypeControl
+          onClick={() => handleSwitchStereoViewClick()}
+        />
+        <FeatureGroup ref={rightImageRef} />
       </LeafletMap>
     </div>
   );
